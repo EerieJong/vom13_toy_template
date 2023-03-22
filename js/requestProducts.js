f710d04483816681e4a73f00c3da5e39136aba77
VomEdu <VomEdu@G15.local>
2023-03-22 12:42:09 +0900

    insta section 0322

---
+++ js/requestProducts.js
@@ -0,0 +1,45 @@
+const products = document.querySelector('.products');
+// console.log(products);
+
+const getProductData = async () => {
+  await fetch('../products.json')
+    .then((response) => response.json())
+    .then((data) => {
+      let dataElmt;
+      data.map((item) => {
+        console.log(item);
+        dataElmt = `
+        <div class="product-frame">
+          <div class="product-item">
+            <img src="images/${item.pro_img}" alt="">
+            <div class="product-text">
+              <h4>${item.pro_name}</h4>
+              <strong>&#8361;${item.pro_price
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</strong>
+              <p>${item.pro_desc}</p>
+              <a href="#" class="common-btn">자세히 보기</a>
+            </div>
+          </div>
+        </div>        
+        `;
+        products.innerHTML += dataElmt;
+      });
+      showLimitItems();
+    })
+    .catch((err) => console.log(err));
+};
+
+getProductData();
+
+function showLimitItems(){
+  const pr = document.querySelectorAll('.product-frame');
+  pr.forEach((item) => {
+    item.style.display = 'none';
+  });
+
+  for (let i = 0; i < 3; i++){
+    pr[i].style.display = 'block';
+  }
+  }
+
--- js/userInterface.js
+++ js/userInterface.js
@@ -3,8 +3,7 @@ const header = document.querySelector('#header');
 
 const stickyHeader = () => {
   const scry = window.scrollY;
-  // console.log(scry);
-  if(scry > 0){
+  if (scry > 0) {
     header.classList.add('active');
   } else {
     header.classList.remove('active');
@@ -13,19 +12,41 @@ const stickyHeader = () => {
 
 window.addEventListener('scroll', stickyHeader);
 
-//Mobile Menu Toggle
+// Mobile Menu Toggle
 const mobileBtn = document.querySelector('.mobile-btn');
 
-toggleMobileBtn =(e) => {
-  const target = e.currentTarget; // 2. 함수가 실행되면 현재 클릭한 요소를 targer변수에 저장 
+toggleMobileBtn = (e) => {
+  const target = e.currentTarget; // 2. 함수가 실행되면 현재 클릭한 지점의 요소를 target변수에 저장
   // console.log(target); // 3. target 변수 출력
   target.classList.toggle('active');
 
-  if(target.classList.contains('active') /*active가 있다면*/){
-    target.classList.remove('not-active'); /*not-active를 없애준다*/
+  if (target.classList.contains('active') /* active가 있다면 */) {
+    target.classList.remove('not-active'); /* not-active를 없애준다 */
   } else {
-    target.classList.add('not-active'); /*not-active를 추가해준다*/
+    target.classList.add('not-active'); /* not-active를 추가해준다 */
   }
 };
 
-mobileBtn.addEventListener('click', toggleMobileBtn); // 1. 클릭했을 때 toggleMobileBtn 함수 실행
+mobileBtn.addEventListener('click', toggleMobileBtn); // 1. 클릭했을때 toggleMobileBtn 함수 실행
+
+// console.log($('#h'));
+
+function fitHeight(){
+  const originalHeight = $('#h').height();
+  $('#fh').height(originalHeight);
+}
+
+fitHeight();
+
+$(window).on('resize', function(){
+fitHeight();
+});
+
+// const originalHeight = $('#h').height();
+// $('#fh').height(originalHeight);
+// // console.log(originalHeight);
+// $(window).on('resize', function(){
+//   const originalHeight = $('#h').height();
+//   $('#fh').height(originalHeight);
+//   // console.log(originalHeight);
+// });
